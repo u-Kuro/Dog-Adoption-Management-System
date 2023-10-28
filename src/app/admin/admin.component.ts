e787ac17b8904e6f60e5900cf5286cf4d1bc4a14
u-Kuro <rcdupitas@gmail.com>
2023-10-28 09:38:54 +0800

    fix some functions

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -12,10 +12,11 @@ import { DogAdoptionService } from '../dog-adoption.service';
 })
 export class AdminComponent {
   dogs: Dog[] = []
-  User: User = new User
-  pendingAdoption: PendingAdoption[] = []
-  putDog: Dog = new Dog
-  dogBirthDay: Date | undefined
+  user: User = new User
+  pendingAdoptions: PendingAdoption[] = []
+  inputDog: Dog = new Dog
+  inputDogBirthDay: Date | undefined
+
   constructor(
     private dogAdoptionService: DogAdoptionService,
     private route: ActivatedRoute
@@ -23,18 +24,10 @@ export class AdminComponent {
 
   ngOnInit(): void {
     this.dogAdoptionService.GetPeople().subscribe((data: User[]) => {
-      this.User = data[0]
+      this.user = data[0]
       if (data[0]) {
-        this.User = data[0]
-        this.dogAdoptionService.GetAllDogs().subscribe((data: Dog[]) => {
-          this.dogs = data
-          data.forEach((dog: Dog) => {
-            this.dogAdoptionService.IsDogAvailable(dog.id).subscribe((data) => {
-              dog.status = data ? "Available" : "Pending"
-            })
-          })
-        })
-        this.dogAdoptionService.GetPendingAdoptions().subscribe((data: PendingAdoption[]) => { this.pendingAdoption = data })
+        this.user = data[0]
+        this.refreshData()
       } else {
         this.dogAdoptionService.AddPerson({
           firstName: "dummy",
@@ -43,44 +36,55 @@ export class AdminComponent {
           password: ""
         }).subscribe(() => {
           this.dogAdoptionService.GetPeople().subscribe((data: User[]) => {
-            this.User = data[0]
+            this.user = data[0]
             if (data[0]) {
-              this.User = data[0]
+              this.user = data[0]
             }
-            this.dogAdoptionService.GetAllDogs().subscribe((data: Dog[]) => {
-              this.dogs = data
-              data.forEach((dog: Dog) => {
-                this.dogAdoptionService.IsDogAvailable(dog.id).subscribe((data) => {
-                  dog.status = data ? "Available" : "Pending"
-                })
-              })
-            })
-            this.dogAdoptionService.GetPendingAdoptions().subscribe((data: PendingAdoption[]) => { this.pendingAdoption = data })
+            this.refreshData()
           })
         })
       }
     })
-
   }
 
-  public delete(event: Event, dog: Dog) {
-    event.stopPropagation()
-    this.dogAdoptionService.DeleteDog(dog.id).subscribe(() => {
-      this.dogAdoptionService.GetAllDogs().subscribe((data: Dog[]) => {
-        this.dogs = data
-        data.forEach((dog: Dog) => {
-          this.dogAdoptionService.IsDogAvailable(dog.id).subscribe((data) => {
-            dog.status = data ? "Available" : "Pending"
+  public refreshData() {
+    this.dogAdoptionService.GetAllDogs().subscribe((dogs: Dog[]) => {
+      this.dogAdoptionService.GetPendingAdoptions().subscribe((pendingAdoptions: PendingAdoption[]) => {
+        this.dogs = dogs
+        this.pendingAdoptions = pendingAdoptions
+        dogs.forEach((dog: Dog) => {
+          this.dogAdoptionService.IsDogAvailable(dog.id).subscribe((isAvailable) => {
+            if (isAvailable) {
+              dog.status = "Available"
+            } else {
+              dog.status = "Pending"
+              for (let pendingAdoption of this.pendingAdoptions) {
+                console.log(pendingAdoption.dogId, dog.id, pendingAdoption)
+                if (dog.id === pendingAdoption.dogId) {
+                  pendingAdoption.dog = dog
+                  this.dogAdoptionService.GetPersonById(pendingAdoption.userId).subscribe((person) => {
+                    pendingAdoption.user = person
+                  })
+                  break;
+                }
+              }
+            }
           })
         })
       })
-      this.dogAdoptionService.GetPendingAdoptions().subscribe((data: PendingAdoption[]) => { this.pendingAdoption = data })
     })
   }
 
-  public save(event: Event, dog: Dog) {
+  public deleteDog(event: Event, dog: Dog) {
     event.stopPropagation()
-    if (!this.putDog.name && !this.putDog.breed && !this.dogBirthDay) {
+    this.dogAdoptionService.DeleteDog(dog.id).subscribe(() => {
+      this.refreshData()
+    })
+  }
+
+  public saveDog(event: Event, dog: Dog) {
+    event.stopPropagation()
+    if (!this.inputDog.name && !this.inputDog.breed && !this.inputDogBirthDay) {
       return alert("Please fill one of the fields.")
     }
     let savedDog: {
@@ -92,32 +96,24 @@ export class AdminComponent {
       breed: undefined,
       birthTimestamp: undefined
     };
-    if (this.putDog.name) {
-      savedDog.name = this.putDog.name
+    if (this.inputDog.name) {
+      savedDog.name = this.inputDog.name
     }
-    if (this.putDog.breed) {
-      savedDog.breed = this.putDog.breed
+    if (this.inputDog.breed) {
+      savedDog.breed = this.inputDog.breed
     }
-    if (this.dogBirthDay) {
-      savedDog.birthTimestamp = new Date(this.dogBirthDay).getTime()
+    if (this.inputDogBirthDay) {
+      savedDog.birthTimestamp = new Date(this.inputDogBirthDay).getTime()
       if (savedDog.birthTimestamp > (new Date()).getTime()) {
         return alert("Invalid: birthday was set in the future.")
       }
     }
     this.dogAdoptionService.UpdateDog(dog.id, savedDog).subscribe(() => {
-      this.dogAdoptionService.GetAllDogs().subscribe((data: Dog[]) => {
-        this.dogs = data
-        data.forEach((dog: Dog) => {
-          this.dogAdoptionService.IsDogAvailable(dog.id).subscribe((data) => {
-            dog.status = data ? "Available" : "Pending"
-          })
-        })
-      })
-      this.dogAdoptionService.GetPendingAdoptions().subscribe((data: PendingAdoption[]) => { this.pendingAdoption = data })
+      this.refreshData()
     })
   }
 
-  public add() {
+  public addDog() {
     let savedDog: {
       name: String | undefined,
       breed: String | undefined,
@@ -127,36 +123,42 @@ export class AdminComponent {
       breed: undefined,
       birthTimestamp: undefined
     };
-    if (!(this.putDog.name && this.putDog.breed && this.dogBirthDay)) {
+    if (!(this.inputDog.name && this.inputDog.breed && this.inputDogBirthDay)) {
       return alert("Please fill up all the fields.")
     } else {
-      savedDog.name = this.putDog.name
-      savedDog.breed = this.putDog.breed
-      savedDog.birthTimestamp = new Date(this.dogBirthDay).getTime()
+      savedDog.name = this.inputDog.name
+      savedDog.breed = this.inputDog.breed
+      savedDog.birthTimestamp = new Date(this.inputDogBirthDay).getTime()
       if (savedDog.birthTimestamp > (new Date()).getTime()) {
         return alert("Invalid: birthday was set in the future.")
       }
     }
     this.dogAdoptionService.AddDog(savedDog).subscribe(() => {
-      this.dogAdoptionService.GetAllDogs().subscribe((data: Dog[]) => {
-        this.dogs = data
-        data.forEach((dog: Dog) => {
-          this.dogAdoptionService.IsDogAvailable(dog.id).subscribe((data) => {
-            dog.status = data ? "Available" : "Pending"
-          })
-        })
-      })
-      this.dogAdoptionService.GetPendingAdoptions().subscribe((data: PendingAdoption[]) => { this.pendingAdoption = data })
+      this.refreshData()
     })
   }
 
   public setDogToUpdate(dog: Dog): void {
-    this.putDog.name = dog.name
-    this.putDog.breed = dog.breed
+    this.inputDog.name = dog.name
+    this.inputDog.breed = dog.breed
     if (dog.birthTimestamp) {
-      this.dogBirthDay = new Date(dog.birthTimestamp)
+      this.inputDogBirthDay = new Date(dog.birthTimestamp)
     } else {
-      this.dogBirthDay = undefined
+      this.inputDogBirthDay = undefined
     }
+  }
+
+  public approveAdoption(pendingAdoption: PendingAdoption): void {
+    this.dogAdoptionService.DeletePendingAdoption(pendingAdoption.id).subscribe(() => {
+      this.dogAdoptionService.DeleteDog(pendingAdoption.dogId).subscribe(() => [
+        this.refreshData()
+      ]);
+    });
+  }
+
+  public rejectAdoption(pendingAdoption: PendingAdoption): void {
+    this.dogAdoptionService.DeletePendingAdoption(pendingAdoption.id).subscribe(() => {
+      this.refreshData()
+    });
   }
 }
